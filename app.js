a7e6cc5823e177770456bb788b937d2b990aaf9f
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-09-02 16:39:23 -0500

    setup routes and middleware

--- app.js
+++ app.js
@@ -13,14 +13,23 @@ const connectDB = require("./db/connect");
 // routers
 
 // middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
-const port = process.env.PORT || 4000;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("E-commerce API");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
-        app.listen(() => {
-            console.log(port, `server listening on port ${port}`);
-        });
+        app.listen(port, console.log(`server listening on port ${port}`));
     } catch (error) {
         console.log(error);
     }
