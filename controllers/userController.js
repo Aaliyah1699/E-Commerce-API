39a3b3e6b4de0c45f7ebb545197a028f2b199618
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-09-07 18:44:55 -0500

    Fetch user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,20 @@
+const User = require("../models/User");
+const customError = require("../errors");
+const { StatusCodes } = require("http-status-codes");
+const { custom } = require("joi");
+
 const getAllUsers = async (req, res) => {
-    res.send("all users");
+    const users = await User.find({ role: "user" }).select("-password");
+
+    res.status(StatusCodes.OK).json({ users });
 };
 const getSingleUser = async (req, res) => {
-    res.send("single user");
+    const user = await User.findOne({ _id: req.params.id }).select("-password");
+
+    if (!user) {
+        throw new customError.NotFoundError(`No user with id ${req.params.id}`);
+    }
+    res.status(StatusCodes.OK).json({ user });
 };
 const showCurrentUser = async (req, res) => {
     res.send("current user");
