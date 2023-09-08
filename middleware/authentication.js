246a40ca3c9252151f356cf33955a13169dcff53
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-09-07 19:50:06 -0500

    Authenticate User set up

---
+++ middleware/authentication.js
@@ -0,0 +1,16 @@
+const customError = require("../errors");
+const { isTokenValid } = require("../utils");
+
+const authenticateUser = async (req, res, next) => {
+    const token = req.signedCookies.token;
+
+    if (!token) {
+        console.log("error, no token");
+    } else {
+        console.log("token present");
+    }
+
+    next();
+};
+
+module.exports = authenticateUser;
