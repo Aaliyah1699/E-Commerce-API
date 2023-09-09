fac4d0038708ffc597cbe29ed3d1adb218bb0f24
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-09-09 18:32:46 -0500

    Review controller setup

---
+++ controllers/reviewController.js
@@ -0,0 +1,30 @@
+const Review = require("../models/Review");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+
+const createReview = async (req, res) => {
+    res.send("create review");
+};
+
+const getAllReviews = async (req, res) => {
+    res.send("all reviews");
+};
+const getSingleReview = async (req, res) => {
+    res.send("single review");
+};
+
+const updateReview = async (req, res) => {
+    res.send("update review");
+};
+
+const deleteReview = async (req, res) => {
+    res.send("delete review");
+};
+
+module.exports = {
+    createReview,
+    getAllReviews,
+    getSingleReview,
+    updateReview,
+    deleteReview,
+};
