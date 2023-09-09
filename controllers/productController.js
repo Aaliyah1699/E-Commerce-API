fbd16dc3027661cfbb21c341421f2fe54b1e7a5d
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-09-09 16:45:53 -0500

    refactor create product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,27 +2,30 @@ const Product = require("../models/Product");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 
-const createProduct = (req, res) => {
-    res.send("create product");
+const createProduct = async (req, res) => {
+    req.body.user = req.user.userId;
+    const product = await Product.create(req.body);
+
+    res.status(StatusCodes.CREATED).json({ product });
 };
 
-const getAllProducts = (req, res) => {
+const getAllProducts = async (req, res) => {
     res.send("all products");
 };
 
-const getSingleProduct = (req, res) => {
+const getSingleProduct = async (req, res) => {
     res.send("single product");
 };
 
-const updateProduct = (req, res) => {
+const updateProduct = async (req, res) => {
     res.send("update product");
 };
 
-const deleteProduct = (req, res) => {
+const deleteProduct = async (req, res) => {
     res.send("delete product");
 };
 
-const uploadImage = (req, res) => {
+const uploadImage = async (req, res) => {
     res.send("upload image");
 };
 
