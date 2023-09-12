6bcb5936f7a84e9a6bb22140c4c5c28ffc2584f8
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-09-12 00:19:06 -0500

    Add to schema and create new schema

--- models/Order.js
+++ models/Order.js
@@ -1,22 +1,52 @@
 const mongoose = require("mongoose");
 
+const SingleOrderItemSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    product: {
+        type: mongoose.Schema.ObjectId,
+        ref: "Product",
+        required: true,
+    },
+});
+
 const OrderSchema = mongoose.Schema(
     {
         tax: {
             type: Number,
+            required: true,
         },
         shippingFee: {
             type: Number,
+            required: true,
         },
         subtotal: {
             type: Number,
+            required: true,
         },
         total: {
             type: Number,
+            required: true,
         },
-        orderItems: [],
+        orderItems: [SingleOrderItemSchema],
         status: {
             type: String,
+            enum: ["pending", "failed", "payed", "delivered", "canceled"],
+            default: "pending",
         },
         user: {
             type: mongoose.Schema.ObjectId,
@@ -25,8 +55,9 @@ const OrderSchema = mongoose.Schema(
         },
         clientSecret: {
             type: String,
+            required: true,
         },
-        paymentId: {
+        paymentIntentId: {
             type: String,
         },
     },
