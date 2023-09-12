18e7de78d0f64d166f8df4c8b0c06713f15e9cd9
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-09-11 23:00:01 -0500

    Aggregate pipeline

---
+++ models/temp.js
@@ -0,0 +1,37 @@
+// Aggregation pipeline atlas
+
+import { MongoClient } from "mongodb";
+import { ObjectId } from "mongodb";
+
+/*
+ * Requires the MongoDB Node.js Driver
+ * https://mongodb.github.io/node-mongodb-native
+ */
+
+const agg = [
+    {
+        $match: {
+            product: new ObjectId("64fe652574675da9e35d44e2"),
+        },
+    },
+    {
+        $group: {
+            _id: null,
+            averageRating: {
+                $avg: "$rating",
+            },
+            numOfReviews: {
+                $sum: 1,
+            },
+        },
+    },
+];
+
+const client = await MongoClient.connect("", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const coll = client.db("E-COMMERCE-API").collection("reviews");
+const cursor = coll.aggregate(agg);
+const result = await cursor.toArray();
+await client.close();
