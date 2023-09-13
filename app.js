cb6d733ca91e6a5659aec197d197a05cc6f5a730
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-09-12 21:41:38 -0500

    index and clean up code

--- app.js
+++ app.js
@@ -35,21 +35,10 @@ app.use(cors());
 app.use(xss());
 app.use(expressMongoSanitize());
 
-app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET)); // temp
 app.use(express.static("./public"));
 app.use(fileUpload());
-
-app.get("/", (req, res) => {
-    res.send("E-commerce API");
-});
-// app.get("/api/v1", (req, res) => {
-//     // test cookie
-//     // console.log(req.cookies);
-//     console.log(req.signedCookies);
-//     res.send("E-commerce API");
-// });
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
